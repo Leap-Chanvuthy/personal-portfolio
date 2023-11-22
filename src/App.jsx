d4daeb56523e198f0be55d5471d17e9509587b00
Elon Thy <115206732+Leap-Chanvuthy@users.noreply.github.com>
2023-11-22 15:57:22 +0700

    initialize commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {React} from 'react'
+import Navbar from './components/layouts/Navbar';
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+      <h4 className='text-3xl text-red-500'>Hello vuthy</h4>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/layouts/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+
+            <ul className="flex justify-center items-center gap-6 w-[full] h-[3rem] font-medium">
+                <li>Home</li>
+                <li>About</li>
+                <li>Articles</li>
+                <li>Contact</li>
+                <li>Blog</li>
+            </ul>
+
+     );
+}
+ 
+export default Navbar;
