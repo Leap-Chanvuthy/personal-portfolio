c0f5398461564c8b784e6d2a9c0e7c1ef0a6d7de
Leap-Chanvuthy <chanvuthy.leap@student.cadt.edu.kh>
2025-05-21 12:17:18 +0700

    add : blog detail page

--- src/components/resusable/pagination.jsx
+++ src/components/resusable/pagination.jsx
@@ -2,7 +2,7 @@ import { Pagination } from "flowbite-react";
 
 const GlobalPagination = ({ currentPage, totalPages, onPageChange , itemPerPage , data }) => {
     return (
-        <div>
+        <div className="flex flex-col items-center">
             <div className="text-center">Showing {(currentPage - 1) * itemPerPage + 1} to {Math.min(currentPage * itemPerPage, data.length)} of {data.length} Entries</div>
             <div className="flex overflow-x-auto sm:justify-center">
                 <Pagination
--- src/pages/blog/[id]/page.jsx
+++ src/pages/blog/[id]/page.jsx
@@ -1,8 +1,26 @@
+import { useTranslation } from "react-i18next";
+import Metatag from "../../../components/resusable/meta-tag";
 
 const BlogDetail = () =>{
+
+    const {t} = useTranslation('meta');
+    const richText = `
+        <p>React គឺជាបណ្ណាល័យ JavaScript ប្រើសម្រាប់បង្កើត UI។ វាអាចអភិវឌ្ឍបានលឿន និងអាច scale បាន។ Learn about modern typography, spacing, and color schemes that enhance user experience.</p>
+        <p>Learn about modern typography, spacing, and color schemes that enhance user experience.</p>
+        <ul><li>Use HTTPS</li><li>Rate Limiting</li><li>Authentication & Authorization</li></ul>
+    `;
+
     return (
         <div>
-            Blog Detail page
+            <Metatag title={t('blog.title')} description="Blog Slug"/>
+            <div className="flex flex-col gap-10">
+                <div className="flex flex-col gap-5">
+                    <h1 className="text-2xl font-bold">ការអភិវឌ្ឍន៍ Web App ជាមួយ React</h1>
+                    <p>ការប្រើប្រាស់ React ដើម្បីបង្កើត Web App ប្រកបដោយប្រសិទ្ធភាព</p>
+                </div>
+                <img className="w-full h-[35rem] object-cover rounded-lg" src="https://images.pexels.com/photos/1181675/pexels-photo-1181675.jpeg" />
+                <div dangerouslySetInnerHTML={{ __html: richText }} />
+            </div>
         </div>
     )
 }
